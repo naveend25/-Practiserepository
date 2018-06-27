e01668f029fe9f2ecbdca85e162350da4a18a61e
Naveen Kumar Darshanala <ndarshanala@processmap.ad>
2018-06-27 12:31:58 +0530

    test

--- src/test/java/test/class1.java
+++ src/test/java/test/class1.java
@@ -4,16 +4,8 @@ public class class1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
-		System.out.println("file");
-	System.out.println("test");
-	System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
-System.out.println("test");
-		System.out.println("test");
-System.out.println("new branch");
-		// TODO Auto-generated method stub
+		System.out.println("New branch hello");
+		
 
 	}
 
